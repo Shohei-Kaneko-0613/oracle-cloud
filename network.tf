768c33d283fde38a5c7f75e1aa7c14be558a9df8
Shohei-Kaneko-0613 <38118687+Shohei-Kaneko-0613@users.noreply.github.com>
2020-12-21 14:11:47 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "oci_core_vcn" "PVCN" {
     cidr_block     = "10.0.0.0/16"
-    compartment_id = "${var.compartment-ocid}"
+    compartment_id = "${var.compartment_ocid}"
     display_name   = "PVCN"
     lifecycle {
         prevent_destroy = true
