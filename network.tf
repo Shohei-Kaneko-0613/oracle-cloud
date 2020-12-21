8d6111616cb293663f5fb644b76294cf1305784f
Shohei-Kaneko-0613 <38118687+Shohei-Kaneko-0613@users.noreply.github.com>
2020-12-21 14:23:56 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "oci_core_vcn" "PVCN" {
     cidr_block     = "10.0.0.0/16"
-    compartment_id = "${var.compartmnet_ocid}"
+    compartment_id = "${var.compartment_ocid}"
     display_name   = "PVCN"
 }
