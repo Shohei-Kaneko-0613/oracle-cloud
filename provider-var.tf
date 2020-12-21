0e8b786da518228c6457b40b0a61783bd73ea5ea
Shohei-Kaneko-0613 <38118687+Shohei-Kaneko-0613@users.noreply.github.com>
2020-12-21 14:07:40 +0900

    all update

--- provider-var.tf
+++ provider-var.tf
@@ -1,15 +1,6 @@
-variable "tenancy_ocid" {
-    default = "ocid1.tenancy.oc1..aaaaaaaab7oola5du62f6j2pmxjmk4cczn6ntki7nsjvkg72c552nb5bfvba"
-}
-variable "user_ocid" {
-    default = "ocid1.user.oc1..aaaaaaaapqtfzldyal4oz53jm3e3b5fmymbdmi3ci2dkav7xoeop46foh4ea"
-}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
-
-variable "region" {
-    default = "ap-tokyo-1"
-}
-variable "compartment-ocid" {
-    default ="ocid1.compartment.oc1..aaaaaaaastezteojwm6xsfej5ehfsowg5fqk3vb2uywnpqz6emxn4k4ahsca"
-}
+variable "region" {}
+variable "compartment-ocid" {}
